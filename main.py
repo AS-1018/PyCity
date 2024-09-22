645bc22393ca6e071efae2666016d040a5388056
AS-1018 <oxygenmus08@gmail.com>
2024-09-22 17:50:24 +0300

    Alpha 0.1
    Изменена система взаимодействий с постройками

--- main.py
+++ main.py
@@ -3,15 +3,35 @@ from math import *
 from economic import *
 from population import *
 from graphics import *
-from array import *
 
 money = 0
 hod = 1
-house_num = array('i', [3, 0, 0])
-house1 = array('i', [4, 5, 2])
-house2 = array('i', [15, 8, 4])
-house3 = array('i', [30, 15, 6])
-shop = array('i', [10, 1])
+build_num = {     # кол-во зданий
+    'h1_num': 3,
+    'h2_num': 0,
+    'h3_num': 0,
+    'sh_num': 0,
+}
+house1 = {        # характеристика 1-эт дома
+    'pop': 4,
+    'prof': 5,
+    'def': 2,
+}
+house2 = {        # характеристика 3-эт дома
+    'pop': 15,
+    'prof': 8,
+    'def': 4,
+}
+house3 = {        # характеристика 5-эт дома
+    'pop': 30,
+    'prof': 15,
+    'def': 6,
+}
+shop = {          # характеристика магазина
+    'pop': 0,
+    'prof': 10,
+    'def': 12,
+}
 houses = 0
 shops = 0
 pop_max = 12
@@ -19,7 +39,6 @@ s_price_buy = 22
 s_price_sell = 12
 b_price_buy = 16
 b_price_sell = 10
-# рассчитываемые переменные
 pop = 6
 profit = 0
 defit = 0
@@ -31,21 +50,21 @@ h_price_3 = h3_func(s_price_buy, b_price_buy)
 sh_price = sh_func(s_price_buy, b_price_buy)
 
 logo_draw()
-print('\nPyCity Alpha 0.1r21.2 by A/S')
+print('\nPyCity Alpha 0.1 by A/S')
 name = input('Введите название вашего города:')
 print('Введите "команды",что бы увидеть список доступных действий и пояснения к ним')
 while True:
 
     # рассчёт заработка
-    profit = pr_func(house1[1], house2[1], house3[1], shop[0],
-                     house_num[0], house_num[1], house_num[2], shops)
-    defit = df_func(house1[2], house2[2], house3[2], shop[1],
-                    house_num[0], house_num[1], house_num[2], shops)
+    profit = pr_func(house1['prof'], house2['prof'], house3['prof'], shop['prof'],
+                     build_num['h1_num'], build_num['h2_num'], build_num['h3_num'], shops)
+    defit = df_func(house1['def'], house2['def'], house3['def'], shop['def'],
+                    build_num['h1_num'], build_num['h2_num'], build_num['h3_num'], shops)
     cash = profit-defit
 
     # рассчёт населения
-    pop_max = pm_func(house1[0], house2[0], house3[0],
-                      house_num[0], house_num[1], house_num[2])
+    pop_max = pm_func(house1['pop'], house2['pop'], house3['pop'],
+                      build_num['h1_num'], build_num['h2_num'], build_num["h3_num"])
     pop = pop_func(shops, cash, pop, pop_max)
 
     # ввод действия
@@ -58,32 +77,72 @@ while True:
 
         # список команд
         case "КОМАНДЫ":
-            print("Построить дом/1/3/5 - Построить 1/3/5-этажный дом")
-            print("Цена дома/1/3/5 - Цена 1/3/5-этажного дома")
+            print("\nПостроить дом-1/3/5 - Построить 1/3/5-этажный дом")
+            print("Цена дома-1/3/5 - Цена 1/3/5-этажного дома")
             print(
-                "Построить магазин \nЦена магазина \nСтатистика \nБиржа \nСледущий ход(сх)")
+                "Построить магазин \nЦена магазина \nСтатистика \nБиржа \nСледущий ход(сх)\n")
+
+        # Дом-1
+        case "ПОСТРОИТЬ ДОМ-1":
+            if money >= h_price_1:
+                build_num['h1_num'] = build_num['h1_num'] + 1
+                print("1-этажный дом построен")
+            else:
+                print(f"Недостаточно денег,  не хватает {h_price_1 - money}")
+
+        case "ЦЕНА ДОМ-1": print(f"Сейчас цена 1-этажного дома составляет {h_price_1}")
+
+        # Дом-3
+        case "ПОСТРОИТЬ ДОМ-3":
+            if money >= h_price_1:
+                build_num['h2_num'] = build_num['h2_num'] + 1
+                print("3-этажный дом построен")
+            else:
+                print(f"Недостаточно денег, не хватает {h_price_2 - money}")
+
+        case "ЦЕНА ДОМ-3": print(f"Сейчас цена 3-этажного дома составляет {h_price_2}")
+
+        # Дом-5
+        case "ПОСТРОИТЬ ДОМ-5":
+            if money >= h_price_1:
+                build_num['h3_num'] = build_num['h3_num'] + 1
+                print("5-этажный дом построен")
+            else:
+                print(f"Недостаточно денег,  не хватает {h_price_3 - money}")
+
+        case "ЦЕНА ДОМ-5": print(f"Сейчас цена 5-этажного дома составляет {h_price_3}")
+
+        # Магазин
+        case "ПОСТРОИТЬ МАГАЗИН":
+            if money >= h_price_1:
+                build_num['sh_num'] = build_num['sh_num'] + 1
+                print("Магазин построен построен")
+            else:
+                print(f"Недостаточно денег,  не хватает {sh_price - money}")
+
+        case "ЦЕНА МАГАЗИНА": print(f"Сейчас цена магазина составляет {sh_price}")
 
         # открытие биржы
         case 'БИРЖА':
             print(
-                f"Цены на землю \nПокупка:{s_price_buy} \nПродажа:{s_price_sell}")
+                f"\nЦены на землю \nПокупка:{s_price_buy} \nПродажа:{s_price_sell}")
             print(
-                f"Цены на строй. материалы \nПокупка:{b_price_buy} \nПродажа:{b_price_sell}")
+                f"Цены на строй. материалы \nПокупка:{b_price_buy} \nПродажа:{b_price_sell}\n")
 
         case 'СТАТИСТИКА':
             # рассчёт статы (при каждом запросе считатетсЯ заново)
-            for i in range(len(house_num)):
-                houses += house_num[i]
+            houses = build_num['h1_num'] + \
+                build_num['h2_num'] + build_num['h3_num']
         # вывод статы
             print(
-                f"Доходы:{profit} \nРасходы:{defit} \nЗаработок{cash} \nКол-во всех домов:{houses}")
+                f"\nДоходы:{profit} \nРасходы:{defit} \nЗаработок:{cash} \nКол-во всех домов:{houses}")
             print(
-                f"Кол-во 1-этажных домов:{house_num[0]} \nКол-во 3-этажных домов:{house_num[1]}")
+                f"Кол-во 1-этажных домов:{build_num['h1_num']} \nКол-во 3-этажных домов:{build_num['h2_num']}")
             print(
-                f"Кол-во 5-этажных домов:{house_num[2]} \nКол-во магазинов:{shops}")
+                f"Кол-во 5-этажных домов:{build_num['h3_num']} \nКол-во магазинов:{build_num['sh_num']}\n")
 
         case 'СЛЕДУЩИЙ ХОД' | 'СХ':
-            console_clear
+           # console_clear \не работает
             houses = 0
             hod += 1
             money += cash

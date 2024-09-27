bb04d78e1dba83d5ad7a0205062ce228d97a900b
A/S <oxygenmus08@gmail.com>
2024-09-27 23:06:13 +0300

    Alpha 1.1r27
    - Добавлена нерабочая статусная система
    - Исправлен баг с деньгами при постройке
    
    -Обнаружен баг: при постройке магазина не изменяется доход

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from math import *
 from economic import *
 from population import *
 from graphics import *
+from statistic import *
 
 money = 0
 hod = 1
@@ -16,21 +17,25 @@ house1 = {        # характеристика 1-эт дома
     'pop': 4,
     'prof': 5,
     'def': 2,
+    'lvl': 10,
 }
 house2 = {        # характеристика 3-эт дома
     'pop': 15,
     'prof': 8,
     'def': 4,
+    'lvl': 15,
 }
 house3 = {        # характеристика 5-эт дома
     'pop': 30,
     'prof': 15,
     'def': 6,
+    'lvl': 30,
 }
 shop = {          # характеристика магазина
     'pop': 0,
     'prof': 10,
-    'def': 12,
+    'def': 2,
+    'lvl': 20,
 }
 houses = 0
 shops = 0
@@ -42,18 +47,33 @@ b_price_sell = 10
 pop = 6
 profit = 0
 defit = 0
+st = ''
 
 # инициализация стартовых цен на дома
 h_price_1 = h1_func(s_price_buy, b_price_buy)
 h_price_2 = h2_func(s_price_buy, b_price_buy)
 h_price_3 = h3_func(s_price_buy, b_price_buy)
 sh_price = sh_func(s_price_buy, b_price_buy)
+# определение начального опыта
+lvl = get_lvl(build_num['h1_num'], house1['lvl'], build_num['h2_num'], house2['lvl'],
+              build_num['h3_num'], house3['lvl'], build_num['sh_num'], shop['lvl'],)
+# Определение начвального статуса
+status = get_status(lvl)
+
 
 logo_draw()
-print('\nPyCity Alpha 0.1 by A/S')
+
+print('\nPyCity Alpha 0.1.1r27 by A/S')
 name = input('Введите название вашего города:')
 print('Введите "команды",что бы увидеть список доступных действий и пояснения к ним')
 while True:
+
+    # определение опыта
+    lvl = get_lvl(build_num['h1_num'], house1['lvl'], build_num['h2_num'], house2['lvl'],
+                  build_num['h3_num'], house3['lvl'], build_num['sh_num'], shop['lvl'],)
+
+    # Определение статуса
+    st = get_status(lvl)
 
     # рассчёт заработка
     profit = pr_func(house1['prof'], house2['prof'], house3['prof'], shop['prof'],
@@ -86,6 +106,7 @@ while True:
         case "ПОСТРОИТЬ ДОМ-1":
             if money >= h_price_1:
                 build_num['h1_num'] = build_num['h1_num'] + 1
+                money = money - h_price_1
                 print("1-этажный дом построен")
             else:
                 print(f"Недостаточно денег,  не хватает {h_price_1 - money}")
@@ -96,6 +117,7 @@ while True:
         case "ПОСТРОИТЬ ДОМ-3":
             if money >= h_price_1:
                 build_num['h2_num'] = build_num['h2_num'] + 1
+                money = money - h_price_2
                 print("3-этажный дом построен")
             else:
                 print(f"Недостаточно денег, не хватает {h_price_2 - money}")
@@ -106,6 +128,7 @@ while True:
         case "ПОСТРОИТЬ ДОМ-5":
             if money >= h_price_1:
                 build_num['h3_num'] = build_num['h3_num'] + 1
+                money = money - h_price_3
                 print("5-этажный дом построен")
             else:
                 print(f"Недостаточно денег,  не хватает {h_price_3 - money}")
@@ -116,6 +139,7 @@ while True:
         case "ПОСТРОИТЬ МАГАЗИН":
             if money >= h_price_1:
                 build_num['sh_num'] = build_num['sh_num'] + 1
+                money = money - sh_price
                 print("Магазин построен построен")
             else:
                 print(f"Недостаточно денег,  не хватает {sh_price - money}")
@@ -129,20 +153,9 @@ while True:
             print(
                 f"Цены на строй. материалы \nПокупка:{b_price_buy} \nПродажа:{b_price_sell}\n")
 
-        case 'СТАТИСТИКА':
-            # рассчёт статы (при каждом запросе считатетсЯ заново)
-            houses = build_num['h1_num'] + \
-                build_num['h2_num'] + build_num['h3_num']
-        # вывод статы
-            print(
-                f"\nДоходы:{profit} \nРасходы:{defit} \nЗаработок:{cash} \nКол-во всех домов:{houses}")
-            print(
-                f"Кол-во 1-этажных домов:{build_num['h1_num']} \nКол-во 3-этажных домов:{build_num['h2_num']}")
-            print(
-                f"Кол-во 5-этажных домов:{build_num['h3_num']} \nКол-во магазинов:{build_num['sh_num']}\n")
+        case 'СТАТИСТИКА': get_stat(build_num['h1_num'], build_num['h2_num'], build_num['h3_num'], build_num['sh_num'], profit, defit, cash, lvl, status)
 
         case 'СЛЕДУЩИЙ ХОД' | 'СХ':
-           # console_clear \не работает
             houses = 0
             hod += 1
             money += cash
@@ -156,6 +169,11 @@ while True:
             h_price_2 = h2_func(s_price_buy, b_price_buy)
             h_price_3 = h3_func(s_price_buy, b_price_buy)
             sh_price = sh_func(s_price_buy, b_price_buy)
+            # определение опыта
+            lvl = get_lvl(build_num['h1_num'], house1['lvl'], build_num['h2_num'], house2['lvl'],
+                          build_num['h3_num'], house3['lvl'], build_num['sh_num'], shop['lvl'],)
+            # Определение статуса
+            st = get_status(lvl)
 
         case other:
             print('Такой команды не существует,проверьте,правильно ли ввели команду')
